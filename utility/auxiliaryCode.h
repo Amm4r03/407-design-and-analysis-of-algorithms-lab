5cf4660c751220c10d7f18021e25e5706521c2e7
ammar <akidwai03@gmail.com>
2024-05-18 22:03:06 +0530

    renamed file to .h extension

---
+++ utility/auxiliaryCode.h
@@ -0,0 +1,15 @@
+// adding auxiliary functions here
+#include <stdio.h>
+
+// names pretty self explanatory
+void printArray(int *array, int size)
+{
+    for (int i = 0; i < size; i++) printf("%d ", array[i]);
+    printf("\n");
+}
+
+// yeah
+void swapValues(int *a, int *b)
+{
+    int temp = *a; *a = *b; *b = temp;
+}
